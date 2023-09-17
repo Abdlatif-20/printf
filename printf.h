96385eb62b45fd3492cff6d758eab3fb2ee0b618
Saadbelh <saad.belhimer@gmail.com>
2023-09-16 17:09:59 -0700

    t

--- printf.h
+++ printf.h
@@ -7,12 +7,12 @@
 # include <stdio.h>
 # include <limits.h>
 
-int	_putchar(char c);
-int	_print_num(int num);
-int	_print_string(char *str);
-int	_print_unsigned_int(unsigned int num, int base, int flag);
-int	_print_address(unsigned long long num);
-int	get_len(unsigned int num, int base);
-int	_printf(const char *format, ...);
+int _putchar(char c);
+int _print_num(int num);
+int _print_string(char *str);
+int _print_unsigned_int(unsigned int num, int base, int flag);
+int _print_address(unsigned long long num);
+int get_len(unsigned int num, int base);
+int _printf(const char *format, ...);
 
 # endif
