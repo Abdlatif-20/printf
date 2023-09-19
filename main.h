a9534673b8e1fe96cf9e8b62215a8c2ecf2c1411
Saadbelh <saad.belhimer@gmail.com>
2023-09-19 16:43:35 -0700

    azer

--- main.h
+++ main.h
@@ -6,12 +6,12 @@
 # include <stdio.h>
 # include <limits.h>
 # include "main.h"
-int _putchar(char c);
 int _print_num(int num);
 int _print_string(char *str);
 int _print_unsigned_int(unsigned int num, int base, int flag);
 int _print_address(unsigned long num);
 int get_len(int num, int base);
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 # endif
