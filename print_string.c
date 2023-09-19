0b27d442fd005e206d3a9c479b14417367659a88
Abdlatif-20 <rca8272@gmail.com>
2023-09-19 07:57:00 -0700

    handle string is NULL

--- print_string.c
+++ print_string.c
@@ -10,6 +10,11 @@ int	_print_string(char *str)
 {
 	int count = 0;
 
+	if (!str)
+	{
+		count += write(1, "(null)", 6);
+		return (count);
+	}
 	while (*str)
 		count += write(1, str++, 1);
 	return (count);
