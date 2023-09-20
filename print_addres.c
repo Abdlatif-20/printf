10ec983bd073b78874011555d3ea1680b9a072ad
Saadbelh <saad.belhimer@gmail.com>
2023-09-19 17:04:21 -0700

    uiouio

--- print_addres.c
+++ print_addres.c
@@ -1,12 +1,10 @@
 # include "main.h"
-
 /**
  * _print_address - prints an address to stdout
  * @num: number to print
  *  * Return: number of digits printed
  */
-
-int	_print_address(unsigned long num)
+int _print_address(unsigned long num)
 {
 	int count = 0;
 	static int flag;
@@ -14,22 +12,22 @@ int	_print_address(unsigned long num)
 
 	if (!flag)
 	{
-	_print_string("0x");
-	flag = 1;
+		_print_string("0x");
+		flag = 1;
 	}
 	if (num >= 16)
 	{
-	_print_address(num / 16);
-	_print_address(num % 16);
+		_print_address(num / 16);
+		_print_address(num % 16);
 	}
 	else
 	{
-	if (num < 10)
-	_putchar('0' + num);
-	else
-	{
-	_putchar(hexa[num]);
-	}
+		if (num < 10)
+		_putchar('0' + num);
+		else
+		{
+		_putchar(hexa[num]);
+		}
 	}
 	count = get_len(num, 16);
 	return (count);
